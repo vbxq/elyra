0f7b5deed74ad821645d1324e852c7b5b6c0bb70
vbxq <256484177+vbxq@users.noreply.github.com>
2026-08-09 07:01:48 +0200

    native macros args: v2

--- native-macros/src/args.rs
+++ native-macros/src/args.rs
@@ -7,31 +7,39 @@ use syn::{
 pub struct ModuleArgs {
     pub name: String,
     pub version: Option<String>,
+    pub symbol: Option<String>,
 }
 
 impl Parse for ModuleArgs {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let mut name = None;
         let mut version = None;
+        let mut symbol = None;
 
         let args: Punctuated<ModuleArg, Token![,]> = Punctuated::parse_terminated(input)?;
         for arg in args {
             match arg {
                 ModuleArg::Name(s) => name = Some(s),
                 ModuleArg::Version(s) => version = Some(s),
+                ModuleArg::Symbol(s) => symbol = Some(s),
             }
         }
 
         let name =
             name.ok_or_else(|| syn::Error::new(input.span(), "missing required argument: name"))?;
 
-        Ok(Self { name, version })
+        Ok(Self {
+            name,
+            version,
+            symbol,
+        })
     }
 }
 
 enum ModuleArg {
     Name(String),
     Version(String),
+    Symbol(String),
 }
 
 impl Parse for ModuleArg {
@@ -43,6 +51,7 @@ impl Parse for ModuleArg {
         match ident.to_string().as_str() {
             "name" => Ok(ModuleArg::Name(lit.value())),
             "version" => Ok(ModuleArg::Version(lit.value())),
+            "symbol" => Ok(ModuleArg::Symbol(lit.value())),
             other => Err(syn::Error::new(
                 ident.span(),
                 format!("unknown argument: {}", other),
