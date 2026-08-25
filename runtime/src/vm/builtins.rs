8044397db04f47c65fb99f4bff4fac6ec9689670
vbxq <256484177+vbxq@users.noreply.github.com>
2026-08-25 05:29:16 +0200

    builtins: stage two language surface

--- runtime/src/vm/builtins.rs
+++ runtime/src/vm/builtins.rs
@@ -2,11 +2,7 @@ use super::VM;
 use super::Value;
 use aelys_common::error::RuntimeError;
 
-// core builtins only - everything else is stdlib
 pub fn register_builtins(vm: &mut VM) -> Result<(), RuntimeError> {
-    let type_fn = vm.alloc_native("type", 1, builtin_type)?;
-    vm.set_global("type".to_string(), Value::ptr(type_fn.index()));
-
     let tostring_fn = vm.alloc_native("__tostring", 1, builtin_tostring)?;
     vm.set_global("__tostring".to_string(), Value::ptr(tostring_fn.index()));
 
